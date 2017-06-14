e05caca6b92f3fc409f452d7eb3d5175bb5b8654
itgluck <it.gluck@ya.ru>
2017-06-14 19:53:53 +0300

    version_3.1.1

--- src/pages/page6/page6.ts
+++ src/pages/page6/page6.ts
@@ -15,25 +15,10 @@ import {SocialSharing} from '@ionic-native/social-sharing';
 export class Page6 {
     AlertController; 
     alertCtrl;
-    // Uri:any;
 	constructor(private nav: NavController, alertCtrl: AlertController, private socialSharing: SocialSharing){
 		this.nav = nav;
     this.alertCtrl= alertCtrl;
 
-
-// SocialSaring
-
-
-  // this.socialSharing.canShareViaEmail().then(() => {
-
-  // }).catch(() => {
-
-  // });
-
-  // this.socialSharing.shareViaEmail('Body','Subject', ['gluck007@mail.ru']).then(() => {
-  // }).catch(() =>{
-
-  // });
 
 }
 
@@ -83,25 +68,4 @@ export class Page6 {
    confirm.present();
   }
 
-  // test
-// doShare() {
-// let options = {
-//   message: 'share this', // not supported on some apps (Facebook, Instagram)
-//   subject: 'the subject', // fi. for email
-//   files: ['', ''], // an array of filenames either locally or remotely
-//   url: 'https://www.website.com/foo/#bar?a=b',
-//   chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
-// }
-
-// let onSuccess = function(result) {
-//   console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
-//   console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
-// }
-
-// let onError = function(msg) {
-//   console.log("Sharing failed with message: " + msg);
-// }
-// }
-
-// window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
 }
